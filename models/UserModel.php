976fae494ed008909f533884987e90a3fa9273f4
Luant <luanluong960@gmail.com>
2025-09-20 08:38:28 +0700

    edit insert user

--- models/UserModel.php
+++ models/UserModel.php
@@ -65,12 +65,15 @@ class UserModel extends BaseModel {
      * @return mixed
      */
     public function insertUser($input) {
-        $sql = "INSERT INTO `app_web1`.`users` (`name`, `password`) VALUES (" .
-                "'" . $input['name'] . "', '".md5($input['password'])."')";
+    $sql = "INSERT INTO `app_web1`.`users` (`name`, `fullname`, `email`, `type`, `password`) VALUES (" .
+        "'" . $input['name'] . "', " .
+        "'" . (isset($input['fullname']) ? $input['fullname'] : '') . "', " .
+        "'" . (isset($input['email']) ? $input['email'] : '') . "', " .
+        "'" . (isset($input['type']) ? $input['type'] : 'user') . "', " .
+        "'" . md5($input['password']) . "')";
 
-        $user = $this->insert($sql);
-
-        return $user;
+    $user = $this->insert($sql);
+    return $user;
     }
 
     /**
