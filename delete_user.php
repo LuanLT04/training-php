ba7068cf435c60d934d589d42b3ce3ee1b214fff
Luant <luanluong960@gmail.com>
2025-09-25 11:43:56 +0700

    CSRF

---
+++ delete_user.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require_once 'models/UserModel.php';
+$userModel = new UserModel();
+
+// Chỉ xử lý POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Kiểm tra CSRF token
+    if (empty($_POST['csrf_token']) || $_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+        die('CSRF token validation failed');
+    }
+
+    $id = $_POST['id'] ?? null;
+    if ($id) {
+        $userModel->deleteUserById($id); // Delete user
+    }
+}
+
+header('location: list_users.php');
+exit;
